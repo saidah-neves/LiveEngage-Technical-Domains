16bec08f957c37cc030e9de1b8007d44e108789a
Saidah Neves <saidahneves@Saidahs-MacBook-Pro-2.local>
2021-01-20 20:52:03 -0500

    First commit LE

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 4000;
+
+app.get('/', function(req, res) {
+    //res.send('<html><head></head><body><h1>Teams:</h1></body></html>');
+    res.send(['acs-cognitive-team', 'acs-fabric-team', 'acs-appdev-team', 'acs-database-team', 'acs-database-db2', 'acs-compute-team', 'acs-security-team', 'acs-network-team', 'acs-storage-team', 'acs-virt-team', 'acsvpc']);
+});
+
+app.listen(port, () => console.log(['acs-cognitive-team,acs-fabric-team,acs-appdev-team, acs-database-team, acs-database-db2, acs-compute-team, acs-security-team,acs-network-team, acs-storage-team, acs-virt-team, acsvpc']));
