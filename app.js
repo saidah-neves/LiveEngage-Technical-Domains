185184fd328fce3713828e2166edfbfcb878ce84
Steven-Gassert <stevengassert94@gmail.com>
2021-02-04 12:10:20 -0500

    added goat team

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ var app = express();
 var port = process.env.PORT || 4000;
 
 app.get('/', function(req, res) {
-    //res.send('<html><head></head><body><h1>Teams:</h1></body></html>');
-    res.send(['acs-cognitive-team', 'acs-fabric-team', 'acs-appdev-team', 'acs-database-team', 'acs-database-db2', 'acs-compute-team', 'acs-security-team', 'acs-network-team', 'acs-storage-team', 'acs-virt-team', 'acsvpc']);
+    res.send(['acs-cognitive-team', 'acs-fabric-team', 'acs-appdev-team', 'acs-database-team', 'acs-database-db2', 'acs-compute-team', 'acs-security-team', 'acs-network-team', 'acs-storage-team', 'acs-virt-team', 'acsvpc', 'goat-team']);
 });
 
 app.listen(port, () => console.log(['acs-cognitive-team,acs-fabric-team,acs-appdev-team, acs-database-team, acs-database-db2, acs-compute-team, acs-security-team,acs-network-team, acs-storage-team, acs-virt-team, acsvpc']));
