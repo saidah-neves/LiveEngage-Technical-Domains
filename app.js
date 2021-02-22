edf9759f3b6316c0d9d0d4070270bb812dcdd235
Saidah Neves <saidahneves@Saidahs-MacBook-Pro-2.local>
2021-02-22 11:55:41 -0500

    map a keyword to domain

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 var port = process.env.PORT || 4000;
 
-app.get('/', function(req, res) {
-    //res.send('<html><head></head><body><h1>Teams:</h1></body></html>');
-    res.send(['acs-cognitive-team', 'acs-fabric-team', 'acs-appdev-team', 'acs-database-team', 'acs-database-db2', 'acs-compute-team', 'acs-security-team', 'acs-network-team', 'acs-storage-team', 'acs-virt-team', 'acsvpc']);
-});
+let domains = [
+    {
+      "team": 1,
+      "acs-domain": "acs-cognitive",
+      "focus": "service issue and data loss related queries"
+    },
+    {
+      "team": 2,
+      "acs-domain": "acs-fabric",
+      "focus": "service functions"
+    },
+    {
+      "team": 3,
+      "acs-domain": "acs-security",
+      "focus": "service security"
+    }
+  ];
 
-app.listen(port, () => console.log(['acs-cognitive-team,acs-fabric-team,acs-appdev-team, acs-database-team, acs-database-db2, acs-compute-team, acs-security-team,acs-network-team, acs-storage-team, acs-virt-team, acsvpc, goat-team']));
+  //Example request: http://localhost:4000/domains
+  app.get('/domains', function(req, res) {
+    res.send(domains);
+  });
+
+var teamMappings = [
+  {
+   acsCognitive: ['Assistant', 'IAM'],
+   acsFabric: ['OpenShift', 'SQL'],
+}
+];
+
+ //Example request: http://localhost:4000/team/keywordhere
+app.get('/team/:keyword', function(req, res) {
+    var keyword = req.query.keyword;
+    var teamName = teamMappings.find((team) => team.keyword === keyword);
+    res.send(teamName);
+  });
+
+app.listen(port);
