51b70a6da2e0ec98d0fa4a04a9ec2402f7129512
Saidah Neves <saidahneves@Saidahs-MacBook-Pro-2.local>
2021-04-01 09:32:31 -0400

    cleanup

--- app.js
+++ app.js
@@ -1,43 +1,7 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
 
 var port = process.env.PORT || 4000;
-
-let domains = [
-    {
-      "team": 1,
-      "acs-domain": "acs-cognitive",
-      "focus": "service issue and data loss related queries"
-    },
-    {
-      "team": 2,
-      "acs-domain": "acs-fabric",
-      "focus": "service functions"
-    },
-    {
-      "team": 3,
-      "acs-domain": "acs-security",
-      "focus": "service security"
-    }
-  ];
-
-  //Example request: http://localhost:4000/domains
-  app.get('/domains', function(req, res) {
-    res.send(domains);
-  });
-
-    //Example request: http://localhost:4000/domains/3 (or any number)
-    app.get('/domains/:team', function(req, res) {
-      var domainTeam = Number(req.params.team);
-      var getDomain = domains.find((domain) => domain.team === domainTeam);
-  
-      if (!getDomain) {
-        res.status(500).send('Domain not found.')
-      } else {
-      res.send(getDomain);
-      }
-  });
 
   var teamMappings = {
     "acsCognitive": "Assistant",
